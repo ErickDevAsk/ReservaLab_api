cf4484da77efaf360869e9e6a2ebf6c0764f22d8
Christian Peña <b8cepc@gmail.com>
2026-04-24 21:34:30 -0600

    feat(api): nuevo endpoint GET para consultar reservas por laboratorio

--- reservations/urls.py
+++ reservations/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import AprobarReservaView, CrearReservaView, RechazarReservaView
+from .views import AprobarReservaView, CrearReservaView, RechazarReservaView,ListarReservasPorLaboratorioView
 
 urlpatterns = [
     path('crear/', CrearReservaView.as_view(), name='crear_reserva'),
     # 👇 Nuestras nuevas rutas dinámicas que reciben el ID (<int:pk>) de la reserva
     path('<int:pk>/aprobar/', AprobarReservaView.as_view(), name='aprobar_reserva'),
     path('<int:pk>/rechazar/', RechazarReservaView.as_view(), name='rechazar_reserva'),
+    path('laboratorio/<int:laboratorio_id>/', ListarReservasPorLaboratorioView.as_view(), name='listar_reservas_lab'),
 ]
--- reservations/views.py
+++ reservations/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import get_object_or_404
 from accounts.permissions import IsAdminOrTecnico
 from .serializers import CrearReservaSerializer
 from .models import Reserva
+from django.db.models import Q
 
 class CrearReservaView(APIView):
     # Esto asegura que solo usuarios con token puedan hacer la petición
@@ -72,3 +73,30 @@ class RechazarReservaView(APIView):
             "mensaje": f"La reserva de {reserva.usuario.username} ha sido RECHAZADA.",
             "estado": reserva.estado
         }, status=status.HTTP_200_OK)
+    
+class ListarReservasPorLaboratorioView(APIView):
+    permission_classes = [IsAuthenticated] # Los estudiantes deben poder verlas
+
+    def get(self, request, laboratorio_id):
+        # Buscamos reservas del laboratorio que NO estén rechazadas o canceladas
+        # para que el estudiante vea esas horas como "ocupadas"
+        reservas = Reserva.objects.filter(
+            laboratorio_id=laboratorio_id
+        ).filter(
+            Q(estado='Aprobada') | Q(estado='Pendiente')
+        )
+        
+        # Como solo necesitamos la fecha y hora para pintar el calendario verde/rojo,
+        # podemos mandar los datos crudos sin un serializer complejo
+        data = []
+        for r in reservas:
+            data.append({
+                "id": r.id,
+                "fecha": str(r.fecha),
+                "hora_inicio": str(r.hora_inicio),
+                "hora_fin": str(r.hora_fin),
+                "estado": r.estado
+            })
+            
+        return Response(data, status=status.HTTP_200_OK)
+    
