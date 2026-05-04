40e1fe8d61de83176dde4e5d2fbd00d0acdfc9e6
ErickDevAsk <112228747+ErickDevAsk@users.noreply.github.com>
2026-05-04 15:03:24 -0600

    Feat: last changes

--- equipment/views.py
+++ equipment/views.py
@@ -1,10 +1,14 @@
 from rest_framework import viewsets
 from .models import Equipo
 from .serializers import EquipoSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated
 # nuestro cadenero desde accounts
 from accounts.permissions import IsAdminOrTecnico 
 
 class EquipoViewSet(viewsets.ModelViewSet):
     queryset = Equipo.objects.all()
     serializer_class = EquipoSerializer
-    permission_classes = [IsAdminOrTecnico]
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            return [IsAuthenticated]
+        return [IsAdminOrTecnico()]
