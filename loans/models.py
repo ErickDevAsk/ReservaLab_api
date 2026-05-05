57ac81d1084d90d680d3f4846a175dd53b04e454
Christian Peña <b8cepc@gmail.com>
2026-05-04 23:26:43 -0600

    feat(back): actualización automática de stock con signals y campos de solo lectura corregidos

--- loans/models.py
+++ loans/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.conf import settings
 from equipment.models import Equipo
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 class Prestamo(models.Model):
     usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     equipo = models.ForeignKey(Equipo, on_delete=models.CASCADE)
@@ -23,3 +26,18 @@ class Incidencia(models.Model):
     def __str__(self):
         return f"Incidencia: {self.equipo.nombre}"
 
+@receiver(post_save, sender=Prestamo)
+def actualizar_stock_equipo(sender, instance, created, **kwargs):
+    equipo = instance.equipo
+    
+    # Si se acaba de crear la solicitud, descontamos del stock
+    if created:
+        equipo.cantidad_disponible -= instance.cantidad_solicitada
+        equipo.save()
+        
+    # Si el técnico lo rechaza o el estudiante lo devuelve, regresamos el stock
+    elif instance.estado in ['Rechazado', 'Devuelto']:
+        # Ojo: Aquí se puede requerir lógica extra para no sumar dos veces
+        # si se actualiza el mismo registro varias veces, pero es la base.
+        equipo.cantidad_disponible += instance.cantidad_solicitada
+        equipo.save()
--- loans/serializers.py
+++ loans/serializers.py
@@ -1,10 +1,26 @@
 from rest_framework import serializers
-from .models import Prestamo  # 👈 usa tu modelo real
+from .models import Prestamo
 
 class LoanSerializer(serializers.ModelSerializer):
-    usuario_nombre = serializers.CharField(source='usuario.username', read_only=True)
-    equipo_nombre = serializers.CharField(source='equipo.nombre', read_only=True)
+    # Esto sigue igual: asigna el ID en la base de datos
+    usuario = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    
+    equipo_nombre = serializers.ReadOnlyField(source='equipo.nombre')
+    
+    usuario_nombre = serializers.ReadOnlyField(source='usuario.username') 
 
     class Meta:
         model = Prestamo
-        fields = '__all__'
+        fields = [
+            'id', 
+            'usuario', 
+            'usuario_nombre', # <- Lo agregamos a la lista
+            'equipo', 
+            'equipo_nombre',  # <- Lo agregamos a la lista
+            'cantidad_solicitada', 
+            'fecha_prestamo', 
+            'fecha_devolucion_prevista', 
+            'estado'
+        ]
+        read_only_fields = ['id', 'fecha_prestamo']
+
