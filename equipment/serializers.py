aa226fa68cb581b41b179d82f5e4cd74511bcc90
Christian Peña <b8cepc@gmail.com>
2026-05-06 19:42:45 -0600

    feat: modal de recepcion con registro de incidencias y endpoint para reportes

--- equipment/serializers.py
+++ equipment/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
-from .models import Equipo
+from .models import Equipo, Incidencia
 
 class EquipoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Equipo
         fields = '__all__'
+
+class IncidenciaSerializer(serializers.ModelSerializer):
+    # Esto es para que en Angular podamos leer el nombre del equipo y no solo el ID número 3
+    equipo_nombre = serializers.ReadOnlyField(source='equipo.nombre')
+    
+    class Meta:
+        model = Incidencia
+        fields = ['id', 'equipo', 'equipo_nombre', 'descripcion', 'fecha_reporte', 'estado']
+
